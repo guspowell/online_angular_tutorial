a99e52cb28dd93f78e2b0a0ab5e754a559cbf45b
guspowell <augustuspowell@me.com>
2015-04-09 16:05:38 +0100

    setting more scope variables

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,5 +1,9 @@
 app.controller('MainController', ["$scope", function($scope) {
 
-  $scope.title = 'Top Sellers in Books'
+  $scope.title = 'Top Sellers in Books';
+
+  $scope.promo = 'i am awesome';
+
+  $scope.product = { name: 'The Book of Trees', price: 19 }
 
 }]);
