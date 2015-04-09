f7785d9c97456d69743bcd49159a042c2e2d0ecd
guspowell <augustuspowell@me.com>
2015-04-09 17:23:04 +0100

    added dislike button

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -10,33 +10,41 @@ app.controller('MainController', ["$scope", function($scope) {
       price: 19,
       pubdate: new Date('2014', '03', '08'),
       cover: 'img/the-book-of-trees.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     {
       name: 'Program or be Programmed',
       price: 8,
       pubdate: new Date('2013', '08', '01'),
       cover: 'img/program-or-be-programmed.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     {
       name: 'Harry Potter',
       price: 12,
       pubdate: new Date('2005', '08', '01'),
       cover: 'img/harry-potter.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     {
       name: 'Freakenomics',
       price: 10,
       pubdate: new Date('2008', '08', '01'),
       cover: 'img/freakenomics.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     }
   ];
 
   $scope.plusOne = function(index) {
     $scope.products[index].likes += 1;
+  };
+
+  $scope.minusOne = function(index) {
+  	products[index].dislikes += 1;
   };
 
 }]);
