69dadaab4d55ec2f5788773e675dccd84974863f
guspowell <augustuspowell@me.com>
2015-04-09 17:11:28 +0100

    added a plusOne function to increase likes

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -9,14 +9,34 @@ app.controller('MainController', ["$scope", function($scope) {
       name: 'The Book of Trees',
       price: 19,
       pubdate: new Date('2014', '03', '08'),
-      cover: 'img/the-book-of-trees.jpg'
+      cover: 'img/the-book-of-trees.jpg',
+      likes: 0
     },
     {
       name: 'Program or be Programmed',
       price: 8,
       pubdate: new Date('2013', '08', '01'),
-      cover: 'img/program-or-be-programmed.jpg'
+      cover: 'img/program-or-be-programmed.jpg',
+      likes: 0
+    },
+    {
+      name: 'Harry Potter',
+      price: 12,
+      pubdate: new Date('2005', '08', '01'),
+      cover: 'img/harry-potter.jpg',
+      likes: 0
+    },
+    {
+      name: 'Freakenomics',
+      price: 10,
+      pubdate: new Date('2008', '08', '01'),
+      cover: 'img/freakenomics.jpg',
+      likes: 0
     }
-  ]
+  ];
+
+  $scope.plusOne = function(index) {
+    $scope.products[index].likes += 1;
+  };
 
 }]);
